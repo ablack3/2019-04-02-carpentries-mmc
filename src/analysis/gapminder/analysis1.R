4fd3fe85495ca8bd815bfda78dc25d2d6c02bf3c
Adam Black <ablack3@gmail.com>
2019-04-02 18:40:54 +0000

    added a bit of analysis

--- src/analysis/gapminder/analysis1.R
+++ src/analysis/gapminder/analysis1.R
@@ -1,5 +1,6 @@
 library(here)
 library(dplyr)
+library(tidyverse)
 
 gapminder <- readr::read_csv(here("data/gapminder/raw/gapminder_data.csv"))
 
@@ -7,3 +8,37 @@ gapminder %>%
   group_by(country) %>% 
   summarize(mean_val = mean(lifeExp)) %>% 
   arrange(mean_val)
+
+
+# Create gap_wide
+gap_wide <- gapminder %>%
+  gather(key = 'key', value = 'value', c('pop', 'lifeExp', 'gdpPercap')) %>%
+  mutate(year_var = paste(key, year, sep = '_')) %>%
+  select(country, continent, year_var, value) %>%
+  spread(key = 'year_var', value = 'value')
+
+
+?all.equal
+all.equal(mtcars, mtcars[, rev(names(mtcars))])
+
+df <- data_frame(a = 1:3, b = 4:6)
+df2 <- df %>% 
+  select(b, a)
+
+all.equal(df, df2)
+class(df)
+
+all.equal
+tibble:::all.equal.tbl_df
+
+?getMethod
+getMethod()
+
+?unite
+
+res <- double(100)
+
+res[1] <- 9
+
+
+
